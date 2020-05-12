b8f0abd881a6dec94d21d0bf2d18938fc17ec60f
Tyler Firth <tyfirth@gmail.com>
2020-05-12 19:29:23 +0000

    Automatically backed up by Learn

--- lib/hello.rb
+++ lib/hello.rb
@@ -1,11 +1,12 @@
 def hello_t(array)
-i = 0 
+ i = 0 
 
 while i < array.length
-yield array[i]
-i = i + 1 
-end
+  yield array[i]
+  i = i + 1 
+ end
 end
 
 # call your method here!
 
+hello_t([])
